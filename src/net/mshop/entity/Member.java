717ef69947ae957ced87f2fab40921a79ff40ec3
panfuhao <pan13557854833@gmail.com>
2016-09-21 17:48:09 +0800

    Initial commit

---
+++ src/net/mshop/entity/Member.java
@@ -0,0 +1,12 @@
+package net.mshop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Panfuhao on 2016/9/21.
+ */
+@Entity
+@Table(name="m_member")
+public class Member extends BaseEntity{
+}
