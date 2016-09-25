c6cf5570965713989606bf7526bf898a2a555b29
pfh695979515@gmail.com <hui5201314>
2016-09-26 07:49:48 +0800

    修改dao

--- src/net/mshop/controller/admin/MemberController.java
+++ src/net/mshop/controller/admin/MemberController.java
@@ -1,12 +1,15 @@
 package net.mshop.controller.admin;
 
 import net.mshop.entity.Member;
+import net.mshop.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 
 /**
  * Created by Panfuhao on 2016/9/23.
@@ -14,9 +17,22 @@ import javax.servlet.http.HttpServletRequest;
 @Controller("adminMemberController")
 @RequestMapping("/admin/member")
 public class MemberController extends BaseController{
+    @Resource(name = "memberServiceImpl")
+    private MemberService memberService;
     @RequestMapping(value = "save" ,method = RequestMethod.POST)
     public String save(Member member, Long memberRankId, HttpServletRequest request, RedirectAttributes redirectAttributes){
-      //  member.setMemberRank();
+        member.setPoint(0L);
+        member.setBalance(BigDecimal.ZERO);
+        member.setAmount(BigDecimal.ZERO);
+        member.setLoginFailureCount(0);
+        member.setLockedDate(null);
+        member.setRegisterIp(request.getRemoteAddr());
+        member.setLoginIp(null);
+        member.setLoginDate(null);
+        member.setOpenId(null);
+        member.setLockKey(null);
+        memberService.save(member);
         return "/admin/member/member";
     }
+
 }
--- src/net/mshop/dao/impl/BaseDaoImpl.java
+++ src/net/mshop/dao/impl/BaseDaoImpl.java
@@ -2,11 +2,34 @@ package net.mshop.dao.impl;
 
 import net.mshop.dao.BaseDao;
 import net.mshop.entity.BaseEntity;
+import org.springframework.core.ResolvableType;
+import org.springframework.util.Assert;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 
 /**
  * Created by Panfuhao on 2016/9/23.
  */
 public abstract class BaseDaoImpl<T extends BaseEntity<ID> ,ID extends Serializable> implements BaseDao<T,ID> {
+    private Class<T> entityClass;
+    @PersistenceContext
+    protected EntityManager entityManager;
+    @SuppressWarnings("unchecked")
+    public BaseDaoImpl() {
+        ResolvableType resolvableType = ResolvableType.forClass(getClass());
+        entityClass = (Class<T>) resolvableType.getSuperType().getGeneric().resolve();
+    }
+    public T findById(ID id) {
+        if (id == null) {
+            return null;
+        }
+        return entityManager.find(entityClass, id);
+    }
+    public void persist(T entity) {
+        Assert.notNull(entity);
+
+        entityManager.persist(entity);
+    }
 }
