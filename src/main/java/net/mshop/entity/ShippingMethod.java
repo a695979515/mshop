26a0a3ecc4b105d06dab11d142661c50222a71c4
a695979515 <pfh13557854833@gmail.com>
2017-03-20 17:59:26 +0800

    发货方式

---
+++ src/main/java/net/mshop/entity/ShippingMethod.java
@@ -0,0 +1,10 @@
+package net.mshop.entity;
+
+/**
+ * 配送方式
+ * Created by Panfuhao on 2017/3/20.
+ */
+public class ShippingMethod extends OrderEntity<Long>{
+    private static final long serialVersionUID = 1354351215645645L;
+    private String name;
+}
